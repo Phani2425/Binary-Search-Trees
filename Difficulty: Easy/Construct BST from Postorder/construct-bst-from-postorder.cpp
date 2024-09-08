10fc88da7f98b1a6b05fb34b7652c9c096493d29
Phani Bhusan Mohanty <mohantyphanibhusan@gmail.com>
2024-09-09 00:35:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Construct BST from Postorder/construct-bst-from-postorder.cpp
@@ -0,0 +1,76 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	int data;
+	struct Node *left;
+	struct Node *right;
+	
+	Node(int x){
+	    data = x;
+	    left = NULL;
+	    right = NULL;
+	}
+};
+
+Node *constructTree (int [], int );
+
+void printInorder (Node* node)
+{
+	if (node == NULL)
+		return;
+	printInorder(node->left);
+	printf("%d ", node->data);
+	printInorder(node->right);
+	
+
+}
+
+int main ()
+{   
+    int t,n;
+   scanf("%d",&t);
+    while(t--){
+        scanf("%d",&n);
+        int post[n];
+        for(int i=0;i<n;i++)
+        scanf("%d",&post[i]);
+
+    Node* root = constructTree(post, n);
+
+	printInorder(root);
+    printf("\n");
+}
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*struct Node
+{
+	int data;
+	Node *left, *right;
+};*/
+
+
+Node * BSTconstructor(int post[],int size, int& index, int Lower, int Upper){
+    //base case
+    if(index < 0 || post[index] < Lower || post[index] > Upper){
+        return NULL;
+    }
+    
+    Node * temp = new Node(post[index--]);
+    temp->right = BSTconstructor(post,size,index,temp->data,Upper);
+    temp->left = BSTconstructor(post,size,index,Lower,temp->data);
+    
+    return temp;
+}
+
+Node *constructTree (int post[], int size)
+{
+  int index = size-1;
+  return BSTconstructor(post,size, index, INT_MIN, INT_MAX);
+}
