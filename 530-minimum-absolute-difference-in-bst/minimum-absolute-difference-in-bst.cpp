046982d3902471a7368541e4d037997ee8516ab6
Phani Bhusan Mohanty <mohantyphanibhusan@gmail.com>
2024-09-29 15:06:21 +0530

    Time: 16 ms (33.58%) | Memory: 24.1 MB (83.71%) - LeetSync

--- 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
+++ 530-minimum-absolute-difference-in-bst/minimum-absolute-difference-in-bst.cpp
@@ -8,35 +8,70 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+//  */
+// class Solution {
+// public:
+//     int getMinimumDifference(TreeNode* root) {
+//         stack<TreeNode*> st;
+//         int minSum = INT_MAX;
+
+//         while(root){
+//             st.push(root);
+//             root = root->left;
+//         }
+
+//         while(!st.empty()){
+//             TreeNode* temp = st.top();
+//             st.pop();
+//             if(temp->right){
+//                 TreeNode* rightNode = temp->right;
+//                 while(rightNode){
+//                     st.push(rightNode);
+//                     rightNode = rightNode->left;
+//                 }
+//             }
+
+//             if(!st.empty()){
+//                 minSum = min(minSum, st.top()->val - temp->val);
+//             }
+//         }
+
+//         return minSum;
+        
+//     }
+// };
+
+//SOLVING THE QUESTION WITHOUT ANY EXTRA SPACE
+//WE WILL DO IT BY MAINTAINING A PREV VALUE WHICH WILL STORE THHE PREVIOUS VALUE OF A NODE IN INORDER TRAVERSAL
+ 
 class Solution {
 public:
+
+    void helperFunction(TreeNode* root,TreeNode* & prev, int & minDiff){
+        if(!root){
+            return;
+        }
+
+        helperFunction(root->left, prev, minDiff);
+        if(prev==NULL){
+            prev = root;
+        }
+        else{
+            minDiff = min(minDiff, (root->val - prev->val));
+            prev = root;
+        }
+        helperFunction(root->right, prev, minDiff);
+
+
+    }
+
     int getMinimumDifference(TreeNode* root) {
-        stack<TreeNode*> st;
-        int minSum = INT_MAX;
+       
+       int minDiff = INT_MAX;
+       TreeNode * prev = NULL;
+       helperFunction(root,prev,minDiff);
 
-        while(root){
-            st.push(root);
-            root = root->left;
-        }
-
-        while(!st.empty()){
-            TreeNode* temp = st.top();
-            st.pop();
-            if(temp->right){
-                TreeNode* rightNode = temp->right;
-                while(rightNode){
-                    st.push(rightNode);
-                    rightNode = rightNode->left;
-                }
-            }
-
-            if(!st.empty()){
-                minSum = min(minSum, st.top()->val - temp->val);
-            }
-        }
-
-        return minSum;
+       return minDiff;
         
     }
 };
